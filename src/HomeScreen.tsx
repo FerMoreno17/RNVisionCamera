0d3816bc2a7d16c3fc42d3eddbf27f4e72d34857
fmoreno <fmoreno@gyf.com.ar>
2022-11-07 13:15:10 -0300

    toma de foto automatica

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from 'react';
 import {
     ActivityIndicator,
@@ -9,9 +10,11 @@ import {
     Text,
     View,
 } from 'react-native';
-import { Camera, CameraDevice, useCameraDevices } from 'react-native-vision-camera';
+import { Camera, CameraDevice, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
 import ImageResizer from '@bam.tech/react-native-image-resizer';
 import { useNavigation } from '@react-navigation/native';
+import { Face, scanFaces } from 'vision-camera-face-detector';
+import { runOnJS } from 'react-native-reanimated';
 //import RNFS from 'react-native-fs';
 
 const HomeScreen = () => {
@@ -22,6 +25,9 @@ const HomeScreen = () => {
     const [permited, setPermited] = useState(false);
     const cameraRef = useRef<Camera>(null);
     const [photoPath, setPhotoPath] = useState<String | undefined>(undefined);
+    let X;
+    let Y;
+    const [desafio, setDesafio] = useState<string>();
 
     const styles = StyleSheet.create({
         container: {
@@ -80,14 +86,6 @@ const HomeScreen = () => {
         },
     });
 
-    useEffect(() => {
-        checkCameraPermission();
-    });
-
-    useEffect(() => {
-        setDeviceSelected(devices.front);
-    }, [devices]);
-
     async function checkCameraPermission() {
         const permission = await Camera.requestCameraPermission();
         if (permission === 'denied') {
@@ -98,6 +96,45 @@ const HomeScreen = () => {
             setPermited(true);
         }
     }
+
+    useEffect(() => {
+        checkCameraPermission();
+    });
+
+    useEffect(() => {
+        setDeviceSelected(devices.front);
+    }, [devices]);
+
+    const frameProcessor = useFrameProcessor((frame) => {
+        'worklet';
+        try {
+            const scannedFaces = scanFaces(frame);
+            console.log(JSON.stringify(scannedFaces, null, 2));
+            if (scannedFaces) {
+                X = scannedFaces[0].pitchAngle;
+                Y = scannedFaces[0].yawAngle;
+                console.log({ Y });
+                console.log({ X });
+                if (Y >= 25) {
+                    runOnJS(handleTakePhoto)('FOTO IZQ');
+                    runOnJS(setDesafio)('Izquierda');
+                }
+                if (Y <= -16) {
+                    runOnJS(handleTakePhoto)('FOTO DER');
+                    runOnJS(setDesafio)('Derecha');
+                }
+                if (X >= 25) {
+                    runOnJS(handleTakePhoto)('FOTO ARR');
+                    runOnJS(setDesafio)('Arriba');
+                }
+                if (X <= -12) {
+                    runOnJS(handleTakePhoto)('FOTO ABJ');
+                    runOnJS(setDesafio)('Abajo');
+                }
+            }
+        }
+        catch (e) { console.log({ e }); }
+    }, []);
 
     if (deviceSelected == null) {
         return (
@@ -117,20 +154,29 @@ const HomeScreen = () => {
         }
     }
 
-    async function cameraTakePhoto() {
-        const photo = await cameraRef.current?.takePhoto({
-            qualityPrioritization: 'speed',
-            enableAutoStabilization: true,
-        });
-        if (photo) {
-            await cropImage(photo.path).then(
-                result => {
-                    setPhotoPath(result);
-                    navigation.navigate('PreviewScreen', { imagePath: result });
-                }
-            ).catch(
-                error => console.log('error ==>', error)
-            );
+    async function handleTakePhoto(tomarFoto: any) {
+        console.log('Captura: ', tomarFoto);
+        let photo;
+        try {
+            photo = await cameraRef.current?.takePhoto({
+                qualityPrioritization: 'speed',
+                enableAutoStabilization: true,
+            });
+
+            if (photo) {
+                await cropImage(photo.path)
+                    .then(
+                        result => {
+                            setPhotoPath(result);
+                            navigation.navigate('PreviewScreen', { imagePath: result });
+                        }
+                    ).catch(
+                        error => console.log('error ==>', error)
+                    );
+            }
+        }
+        catch (e) {
+            console.log({ e });
         }
     }
 
@@ -163,8 +209,6 @@ const HomeScreen = () => {
         return croppedImage;
     }
 
-
-
     return (
         <SafeAreaView style={styles.container}>
             {permited
@@ -177,6 +221,9 @@ const HomeScreen = () => {
                         isActive={true}
                         photo={true}
                         enableZoomGesture
+                        frameProcessor={frameProcessor}
+                        frameProcessorFps={5}
+                        orientation={'portrait'}
                     />
                     <View style={styles.bottomContainer}>
                         <Pressable
@@ -185,8 +232,9 @@ const HomeScreen = () => {
                         >
                             <Text>Flip</Text>
                         </Pressable>
+                        <Text style={{ color: 'black' }}>{desafio}</Text>
                         <Pressable
-                            onPress={cameraTakePhoto}
+                            onPress={handleTakePhoto}
                             style={styles.button}>
                             <Text style={styles.buttonLabel}>
                                 TAKE
